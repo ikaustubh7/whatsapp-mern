0359e236329c3b4df6e59775b69aa4f2331e79ec
Kaustubh <ikaustubh77@gmail.com>
2023-09-30 08:42:07 +0530

    axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import Pusher from "pusher-js";
+import axios from "./axios";
 
 function App() {
+  const [messages , setMessages]=useState([]);
+
+  useEffect(()=>{
+    axios.get('/messages/sync')
+    .then(response => {
+      setMessages(response.data);
+    })
+  },[]);
+
+  useEffect(()=>{
+    //once
+    const pusher = new Pusher('01be9c1ed4d63665bf03', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind("inserted", (newMessage) => {
+      alert(JSON.stringify(newMessage));
+      setMessages([...messages, newMessage]);
+    });
+
+    return ()=>{
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+
+  } ,[ messages]);
+
+  console.log(messages);
+
   return (
     <div className="App">
 
